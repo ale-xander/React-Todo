ba5c41d83ad82f3cca3e02ea2f3a4d5f9e2d73f5
alexiliev <iliev.alex@outlook.com>
2019-08-19 18:14:36 -0700

    added styling. moved the routes in config.

--- my-react-todo/src/App.js
+++ my-react-todo/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import TodosContainer from './containers/TodosContainer';
+import routes from './config/routes'
+import Header from './components/Header';
 
 
 
 function App() {
   return (
     <div className="container">
-      <Switch>
-      <Route exact path='/' component={ Home }/>
-      <Route path='/todos' component={TodosContainer} />
-      </Switch>
+      <Header />
+     {routes}
     </div>
   );
 }
